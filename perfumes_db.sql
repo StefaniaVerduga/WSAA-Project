14acf5ab9ae09b85018ffe9b9b57ff3f7c175684
StefaniaVerduga <g00425680@atu.ie>
2024-05-16 11:05:47 +0100

    update database

--- perfumes_db.sql
+++ perfumes_db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE perfumes_db;
 SHOW databases;
 
 -- Get access to the database
-USE perfumes_db:
+USE perfumes_db;
 
 -- Create the table perfumes within the database
 CREATE TABLE perfumes (
@@ -16,14 +16,14 @@ CREATE TABLE perfumes (
     );
 
 -- Add all the initial data corresponding to the attributes
-INSERT INTO perfumes (id, Name, Brand, Size_ml, Price_eur, Gender) VALUES
-(1, 'Paradoxe', 'Prada', 90, 148, 'F'),
-(2, 'Good Girl', 'Carolina Herrera', 80, 139, 'F'),
-(3, 'Idole', 'Lancome', 100, 141, 'F'),
-(4, 'J’adore', 'Dior', 100, 152, 'F'),
-(5, 'Be Delicious', 'DKNY', 50, 74, 'F'),
-(6, 'Sauvage', 'Dior', 100, 169, 'M'),
-(7, 'Bleu', 'Chanel', 100, 144, 'M'),
-(8, 'Eros', 'Versace', 100, 110, 'M'),
-(9, 'Armani Code', 'Armani', 120, 145, 'M'),
-(10, 'Allure', 'Chanel', 100, 142, 'M');
+INSERT INTO perfumes (Name, Brand, Size_ml, Price_eur, Gender) VALUES
+('Paradoxe', 'Prada', 90, 148, 'F'),
+('Good Girl', 'Carolina Herrera', 80, 139, 'F'),
+('Idole', 'Lancome', 100, 141, 'F'),
+('J’adore', 'Dior', 100, 152, 'F'),
+('Be Delicious', 'DKNY', 50, 74, 'F'),
+('Sauvage', 'Dior', 100, 169, 'M'),
+('Bleu', 'Chanel', 100, 144, 'M'),
+('Eros', 'Versace', 100, 110, 'M'),
+('Armani Code', 'Armani', 120, 145, 'M'),
+('Allure', 'Chanel', 100, 142, 'M');
